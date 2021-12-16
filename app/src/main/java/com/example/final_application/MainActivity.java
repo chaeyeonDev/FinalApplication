abed9ef7b2abdc8eab2c4165ccde6754fbcf1896
chaeyeon <mmlovrr@gmail.com>
2021-12-16 12:45:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/final_application/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.final_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        PersonAdapter adapter = new PersonAdapter();
+
+        adapter.addItem(new Person("책1", "작가1"));
+        adapter.addItem(new Person("책2", "작가2"));
+        adapter.addItem(new Person("책3", "작가3"));
+        adapter.addItem(new Person("책4", "작가4"));
+
+        recyclerView.setAdapter(adapter);
+
+
+
+
+    }
+}
+
+
+
+
